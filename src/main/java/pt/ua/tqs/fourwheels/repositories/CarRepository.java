db2d7505f48d70e47b18695595ecf922a04714c0
lmcosta98 <luismcosta98@gmail.com>
2020-05-20 16:50:52 +0100

    minor change

--- src/main/java/pt/ua/tqs/fourwheels/repositories/CarRepository.java
+++ src/main/java/pt/ua/tqs/fourwheels/repositories/CarRepository.java
@@ -3,8 +3,6 @@ package pt.ua.tqs.fourwheels.repositories;
 import org.springframework.data.repository.CrudRepository;
 import pt.ua.tqs.fourwheels.entities.Car;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
 
 public interface CarRepository extends CrudRepository<Car, Integer> {
 
