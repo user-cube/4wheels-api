54694229ee61037dd675a05a2b2e80ac357fb614
Rui Coelho <ruicoelho@ua.pt>
2020-05-21 15:51:08 +0100

    add logger

--- src/main/java/pt/ua/tqs/fourwheels/authentication/JwtRequestFilter.java
+++ src/main/java/pt/ua/tqs/fourwheels/authentication/JwtRequestFilter.java
@@ -5,6 +5,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +24,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private JwtUserDetailsService jwtUserDetailsService;
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
+    Logger logger = LogManager.getLogger(JwtRequestFilter.class);
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
@@ -34,9 +38,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             try {
                 username = jwtTokenUtil.getUsernameFromToken(jwtToken);
             } catch (IllegalArgumentException e) {
+                logger.info("Unable to get JWT Token");
                 System.out.println("Unable to get JWT Token");
             } catch (ExpiredJwtException e) {
-                System.out.println("JWT Token has expired");
+                logger.info("JWT Token has expired");
             }
         } else {
             logger.warn("JWT Token does not begin with Bearer String");
