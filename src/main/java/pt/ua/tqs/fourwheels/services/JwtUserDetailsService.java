648e15fcc3d4a376cb4b4a90b931eecfedfe33d8
Rui Coelho <ruicoelho@ua.pt>
2020-05-21 15:03:08 +0100

    Authentication Controller

---
+++ src/main/java/pt/ua/tqs/fourwheels/services/JwtUserDetailsService.java
@@ -0,0 +1,40 @@
+package pt.ua.tqs.fourwheels.services;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pt.ua.tqs.fourwheels.models.UserModel;
+import pt.ua.tqs.fourwheels.repositories.Authentication;
+import pt.ua.tqs.fourwheels.entities.Userm;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private Authentication authentication;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Userm user = authentication.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public Userm save(UserModel user) {
+        Userm newUser = new Userm();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        return authentication.save(newUser);
+    }
+}
