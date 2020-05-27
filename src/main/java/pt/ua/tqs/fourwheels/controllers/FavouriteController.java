9b3bccb25bcd02f713ad7f2e9a7613c7427760eb
Rui Coelho <ruicoelho@ua.pt>
2020-05-27 23:18:41 +0100

    return list

--- src/main/java/pt/ua/tqs/fourwheels/controllers/FavouriteController.java
+++ src/main/java/pt/ua/tqs/fourwheels/controllers/FavouriteController.java
@@ -5,10 +5,13 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 import pt.ua.tqs.fourwheels.authentication.JwtTokenUtil;
+import pt.ua.tqs.fourwheels.entities.Car;
 import pt.ua.tqs.fourwheels.entities.Favourite;
 import pt.ua.tqs.fourwheels.repositories.FavouriteRepository;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/favourite")
@@ -30,10 +33,12 @@ public class FavouriteController {
     }
     )
     @GetMapping(value = "/")
-    public Favourite getFavourites(HttpServletRequest request){
+    public List<Favourite> getFavourites(HttpServletRequest request){
         String token = request.getHeader("Authorization").split(" ")[1];
         String email = jwtTokenUtil.getUsernameFromToken(token);
-        return favouriteRepository.findByMail(email);
+        List<Favourite> myList = new ArrayList<>();
+        myList.add(favouriteRepository.findByMail(email));
+        return myList;
     }
     
     @ApiOperation(value = "Delete favourite car by user.", response = Iterable.class)
