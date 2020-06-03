c0828ec9a978e94c69471a4785895e04e778865b
Rui Coelho <ruicoelho@ua.pt>
2020-06-04 00:55:55 +0100

    profile getters e setters

--- src/main/java/pt/ua/tqs/fourwheels/entities/Favourite.java
+++ src/main/java/pt/ua/tqs/fourwheels/entities/Favourite.java
@@ -1,19 +1,10 @@
 package pt.ua.tqs.fourwheels.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class Favourite {
     @Id
@@ -21,4 +12,37 @@ public class Favourite {
     private Integer id;
     private String mail;
     private int car;
+
+    public Favourite(Integer id, String mail, int car) {
+        this.id = id;
+        this.mail = mail;
+        this.car = car;
+    }
+
+    public Favourite() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public int getCar() {
+        return car;
+    }
+
+    public void setCar(int car) {
+        this.car = car;
+    }
 }
