f79563ced53a73a988dfbc29a45c2e136b8abaa1
lmcosta98 <luismcosta98@gmail.com>
2020-06-03 20:05:42 +0100

    number of cars registered by user

--- src/main/java/pt/ua/tqs/fourwheels/controllers/UsersController.java
+++ src/main/java/pt/ua/tqs/fourwheels/controllers/UsersController.java
@@ -11,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pt.ua.tqs.fourwheels.authentication.JwtTokenUtil;
+import pt.ua.tqs.fourwheels.entities.Car;
 import pt.ua.tqs.fourwheels.entities.Profile;
+import pt.ua.tqs.fourwheels.repositories.CarRepository;
 import pt.ua.tqs.fourwheels.repositories.ProfileRepository;
 import springfox.documentation.spring.web.json.Json;
 
@@ -25,6 +27,7 @@ import java.util.Map;
 public class UsersController {
     private ProfileRepository profileRepository;
     private JwtTokenUtil jwtTokenUtil;
+    private CarRepository carRepository;
 
     public UsersController(ProfileRepository profileRepository, JwtTokenUtil jwtTokenUtil){
         this.profileRepository = profileRepository;
@@ -122,5 +125,33 @@ public class UsersController {
     }
 
 
+    @ApiOperation(value = "List all the vendors registered on the platform.", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved all buyers."),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
+    @GetMapping(value = "/vendors/cars/registered")
+    public ResponseEntity<JSONObject> getNumberOfCarsRegisteredByVendor(HttpServletRequest request, @RequestParam(value = "page", required=false) int page, @RequestParam(value = "limit", required=false) int limit){
+        String token = request.getHeader("Authorization").split(" ")[1];
+        String email = jwtTokenUtil.getUsernameFromToken(token);
+        Profile user = profileRepository.findByMail(email);
+        Pageable pageAndLimit = PageRequest.of(page, limit);
 
+        if (user.getType() == 2) {
+            Page<Profile> userPage =  profileRepository.findByOwnerCarsRegistered(pageAndLimit);
+            int totalPages = userPage.getTotalPages();
+            List<Profile> buyersPage = userPage.getContent();
+
+            JSONObject json = new JSONObject();
+            json.put("data", buyersPage);
+            json.put("totalpages", totalPages);
+
+            return ResponseEntity.status(HttpStatus.OK).body(json);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+    }
 }
--- src/main/java/pt/ua/tqs/fourwheels/repositories/ProfileRepository.java
+++ src/main/java/pt/ua/tqs/fourwheels/repositories/ProfileRepository.java
@@ -2,6 +2,7 @@ package pt.ua.tqs.fourwheels.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import pt.ua.tqs.fourwheels.entities.Profile;
@@ -15,6 +16,8 @@ public interface ProfileRepository extends PagingAndSortingRepository<Profile, I
     Profile findByMail(String email);
     @Transactional
     void deleteByMail(String email);
-
     Page<Profile> findAllByTypeEquals(int type, Pageable pageable);
+
+    @Query(value="(SELECT ownerMail as cars, COUNT(*) FROM Car GROUP BY ownerMail ORDER BY 2 DESC)Subquery_1 JOIN Profile ON Subquery_1.ownerMail = Profile.mail", nativeQuery=true)
+    Page<Profile> findByOwnerCarsRegistered(Pageable pageable);
 }
