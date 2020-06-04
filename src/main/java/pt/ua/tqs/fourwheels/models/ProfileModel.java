8c2cad515f5fbed2af9ec822736ce377c205f8ba
João Laranjo <37157325+joaolaranjo@users.noreply.github.com>
2020-06-04 21:51:46 +0100

    ProfileModel

---
+++ src/main/java/pt/ua/tqs/fourwheels/models/ProfileModel.java
@@ -0,0 +1,30 @@
+package pt.ua.tqs.fourwheels.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pt.ua.tqs.fourwheels.entities.Car;
+import pt.ua.tqs.fourwheels.entities.Profile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ProfileModel {
+
+    private Profile profile;
+
+    public ProfileModel(Profile profile) {
+        this.profile = profile;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+}
