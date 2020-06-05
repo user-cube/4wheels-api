2dfb1d920a859c8b424c22e204a37e6e1ded7e37
Iglesias-Leafwind <pedrohpi@ua.pt>
2020-06-05 03:14:35 +0100

    bye bye code smeeeels

--- src/main/java/pt/ua/tqs/fourwheels/models/CarModel.java
+++ src/main/java/pt/ua/tqs/fourwheels/models/CarModel.java
@@ -1,11 +1,6 @@
 package pt.ua.tqs.fourwheels.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 import pt.ua.tqs.fourwheels.entities.Car;
-import pt.ua.tqs.fourwheels.entities.Profile;
 
 
 public class CarModel {
