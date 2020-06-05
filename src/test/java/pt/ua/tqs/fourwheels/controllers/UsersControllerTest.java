742f9cbb375d0e9c4a80766c946a469e8a0f2cda
lmcosta98 <luismcosta98@gmail.com>
2020-06-05 04:32:58 +0100

    fix smell

--- src/test/java/pt/ua/tqs/fourwheels/controllers/UsersControllerTest.java
+++ src/test/java/pt/ua/tqs/fourwheels/controllers/UsersControllerTest.java
@@ -26,7 +26,7 @@ import static org.hamcrest.Matchers.hasKey;
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
